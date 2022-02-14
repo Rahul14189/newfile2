3ab8307c9d0faddff37dd264575b2d420b6c56b0
Rahul jha <rahuljharm@gmail.com>
2022-02-13 22:55:42 -0800

    good

--- smartversion/smartversion/settings.py
+++ smartversion/smartversion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zh0u=416tyc7!8(sn&mcqd828kr9u6*518ntadvwfl((^(2@7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.0', 'sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.0', 'smartversion.herokuapp.com']
 
 
 # Application definition
